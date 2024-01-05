c021e4ecc8ea9648d12f9ea1a092e446e9f09c10
JONEL <jonelteano29@gmail.com>
2024-01-05 15:36:55 +0800

    add hamburger when in mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { Route, Routes } from 'react-router-dom'
 
 // PAGES
 import Home from './pages/Home/Home'
 import Orders from './pages/Orders/Orders';
 import Locations from './pages/Locations/Locations';
-import Customers from './pages/Customers/Customers';
-import Directions from './pages/Dashboard/Directions';
+import Directions from './pages/Directions/Directions';
 
 // COMPONENTS
 import Navbar from './components/sideBar/sideBar';
-
+import Hamburger from './components/hamburgerMenu/Hamburger'
 
 
 import './App.css'
 
 
 function App() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Update isMobile state on window resize
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1065); // Adjust the threshold as needed
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   
   
@@ -34,10 +53,6 @@ function App() {
       element: Locations
     },
     {
-      pathname: "/Customers",
-      element: Customers
-    },
-    {
       pathname: "/Directions/:id/:username",
       element: Directions
     },
@@ -46,7 +61,7 @@ function App() {
 
   return (
     <>
-    <Navbar />
+    {isMobile ? <Hamburger /> : <Navbar />}
       <Routes>
         
         {routes.map((route, index)=> (
