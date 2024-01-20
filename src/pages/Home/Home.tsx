7debadd4d77f0d446846ab68a23e0bc846c85e0b
JONEL <jonelteano29@gmail.com>
2024-01-20 23:06:29 +0800

    implement login auth0

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 
 // COMPONENTS
-import { Input } from "@/components/ui/input"
+import { useAuth0 } from '@auth0/auth0-react'
 
 // PAGE STYLES
 import './Home.css'
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
+
+    const { loginWithRedirect } = useAuth0();
     return (
         <>
             <div className='home-container'>
-                This is Home Page<Input />
+                <h1>Hydromaze Delivery Side</h1>
+                <Button onClick={()=>{ loginWithRedirect()}}>Login</Button>
             </div>
         </>
     )
